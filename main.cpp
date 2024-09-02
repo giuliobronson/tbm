20085f652a5dab0d68c240d751195b2fe981fac9
giuliobronson <giuliobronson@gmail.com>
2024-09-02 15:56:47 -0300

    Função searchKeyWord

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<iostream>
 #define MAX_CONSTS 1000
+
+using namespace std;
 
 typedef enum {
     // Palavras Reservadas
@@ -16,18 +22,18 @@ typedef enum {
     UNKNOWN
 } t_token;
 
-typedef struct {
-    unsigned char type; // 0-char, 1-int, 2-string
-    union {
-        char cVal;
-        int  nVal;
-        char *sVal;
-    };
-} t_const;
+// typedef struct {
+//     unsigned char type; // 0-char, 1-int, 2-string
+//     union {
+//         char cVal;
+//         int  nVal;
+//         string sVal;
+//     };
+// } t_const;
 
-// Vetor de constantes lidas
-t_const vConsts[MAX_CONSTS];
-int nNumConsts = 0;
+// // Vetor de constantes lidas
+// t_const vConsts[MAX_CONSTS];
+// int nNumConsts = 0;
 
 // Caractere lido
 char nextChar = '\x20';
@@ -36,28 +42,44 @@ char nextChar = '\x20';
 t_token token;
 int token2nd;
 
-// Função para verificar tabela de palavras-chave
-t_token searchKeyWord(char *name);
-
 // Função para inserir e buscar identificadores na tabela de nomes
-int searchName(char *name);
+int searchName(string name) {
+    return 4;
+}
+
+// Função para verificar tabela de palavras-chave
+t_token searchKeyWord(string name) {
+    vector<string> keyWordTable = {
+        "alias", "bool", "break", "char", "continue", "do", "else", "false", 
+        "function", "if", "int", "string", "struct", "true", "var", "while"
+    };
+    auto it = lower_bound(keyWordTable.begin(), keyWordTable.end(), name);
+    if(it != keyWordTable.end() && *it == name)
+        return (t_token)(searchName(name));
+    return ID;
+}
 
 // Funções para inclusões de constantes de cada tipo. Retornam a posição em que foram inseridas
 int addCharConst(char c);
 
 int addIntConst(int n);
 
-int addStringConst(char *s);
+int addStringConst(string s);
 
 // Funções para a recuperação do valor de uma constante a partir de sua posição
 char getCharConst(int n);
 
 int getIntConst(int n);
 
-char *getStringConst(int n);
+string getStringConst(int n);
 
 // Função para a leitura do arquivo de entrada
 char readChar(void);
 
 // Função que implementa o autômato finito do analisador léxico
 t_token nextToken(void);
+
+int main() {
+    cout << searchKeyWord("do") << endl;
+    return 0;
+}
